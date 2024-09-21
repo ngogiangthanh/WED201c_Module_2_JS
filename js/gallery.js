4068f4ced79ef217ef250abd6757407e17bcb5dc
Thanh.Ngo <thanhthanh1516@gmail.com>
2024-09-21 23:03:46 +0700

    Add #2 commit

--- js/gallery.js
+++ js/gallery.js
@@ -1,17 +1,17 @@
 /* gallery.js */
 
 function upDate(previewPic) {
-    // 1) Change the background image URL
+    // Change the background to image URL
     document.getElementById('image').style.backgroundImage = `url('${previewPic.src}')`;
 
-    // 2) Change the text content
+    // Change the text to content
     document.getElementById('image').textContent = previewPic.alt;
 }
 
 function unDo() {
-    // 1) Reset background image to the original (empty in this case)
+    // Reset background image to the original
     document.getElementById('image').style.backgroundImage = 'url("")';
 
-    // 2) Reset text content to the original
-    document.getElementById('image').textContent = 'Hover over an image below to display here.';
+    // Reset text content to the original
+    document.getElementById('image').textContent = 'Please hover over an image below to display here!';
 }
